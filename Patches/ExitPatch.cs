a746f042f33d65dd5c3b13ee4f84f3679ae87e95
acidphantasm <acidphantasm@acidphantasm.com>
2025-08-01 20:38:56 -0500

    Add project files.

---
+++ Patches/ExitPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using EFT;
+using EFT.Interactive;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace acidphantasm_letmeout.Patches
+{
+    internal class InteractWithTransit_Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GClass1676), nameof(GClass1676.InteractWithTransit));
+        }
+
+        [PatchPrefix]
+        static bool Prefix(GClass1676 __instance, Player player, TransitInteractionPacketStruct packet)
+        {
+            if (__instance == null) return true;
+
+            Logger.LogInfo("DisableExitsHit");
+
+            __instance.method_15(player);
+            __instance.transitPlayers.Add(player.ProfileId, player.Id);
+            __instance.profileKeys[player.ProfileId] = packet.keyId;
+            __instance.dictionary_0[packet.pointId].GroupEnter(player);
+            //ExfiltrationControllerClass.Instance.BannedPlayers.Add(player.Id);
+            ExfiltrationControllerClass.Instance.CancelExtractionForPlayer(player);
+            //ExfiltrationControllerClass.Instance.DisableExitsInteraction();
+
+            return false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using acidphantasm_letmeout.Patches;
+using BepInEx;
+
+namespace acidphantasm_letmeout
+{
+    [BepInPlugin("com.acidphantasm.letmeout", "acidphantasm-LetMeOut", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new InteractWithTransit_Patch().Enable();
+        }
+    }
+}
